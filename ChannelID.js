650ec60e6d8db19d2e049d427029a9b0523661f0
Rafly Andrian W <raflyandrian@gmail.com>
2026-04-21 13:57:38 +0700

    Add BotChannelID and TestChannelID to ChannelID.js

--- ChannelID.js
+++ ChannelID.js
@@ -1,1 +1,5 @@
-module.exports = {GeneralID: "552167610045956101"} //Masukkan ID Channel dimana ingin menampilkan status bot Online atau Offline 
+module.exports = {
+  GeneralID: "552167610045956101",
+  BotChannelID: "433284871591428097",
+  TestChannelID: "1476388831904137277",
+};
